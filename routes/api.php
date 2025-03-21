54f53f2912749bba5f665974eda767efca959770
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-21 15:37:33 +0000

    ROUTE : api updated with ticket resource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('tickets', TicketController::class);
 });
