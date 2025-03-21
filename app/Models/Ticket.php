f00f2447ac2e55048b3f4e3e402c89d996d05410
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-21 15:38:00 +0000

    MODEL: ticket created and updated with relations

---
+++ app/Models/Ticket.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $fillable = [
+        'subject', 'description', 'customer_id', 'assigned_agent_id', 'status'
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function assignedAgent()
+    {
+        return $this->belongsTo(User::class, 'assigned_agent_id');
+    }
+}
