7321d1a3a10af2e12a1aebdf5af65b943586bfaf
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-21 14:04:48 +0000

    CONTROLLER: auth created

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+
+
+class AuthController extends Controller
+{
+    
+}
