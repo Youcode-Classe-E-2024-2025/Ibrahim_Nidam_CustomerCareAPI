8380eab8c2c5ea8216a2640f4daa99e5bfee20de
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-21 13:44:37 +0000

    MIGRATION: user table updated with role column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('role', ['user', 'agent'])->default('user');
+            $table->string('agent_code')->nullable();
             $table->timestamps();
         });
 
