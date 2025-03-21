db0c9973d0bde52327628f08142ad4a4d8174ada
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-21 14:05:24 +0000

    CONTROLLER: auth updated with swagger info header

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -8,7 +8,13 @@ use App\Http\Requests\Auth\RegisterRequest;
 use App\Services\AuthService;
 use Illuminate\Http\Request;
 
-
+/**
+ * @OA\Info(
+ *     title="CustomerCareAPI",
+ *     version="1.0.0",
+ *     description="Customer Care API for ticket management"
+ * )
+ */
 class AuthController extends Controller
 {
     
