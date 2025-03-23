644553bf20e5262cbaf9ebc017344dfce8d1fca4
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-23 14:38:34 +0000

    PROVIDER: app service updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Response;
+use App\Repositories\ResponseRepository;
 use App\Repositories\TicketRepository;
 use App\Services\TicketService;
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
     
         $this->app->bind(TicketService::class, function ($app) {
             return new TicketService($app->make(TicketRepository::class));
+        });
+
+        $this->app->bind(ResponseRepository::class, function ($app) {
+            return new ResponseRepository($app->make(Response::class));
         });
     }
 
