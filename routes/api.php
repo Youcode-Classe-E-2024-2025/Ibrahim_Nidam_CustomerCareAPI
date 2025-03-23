44f12331f1158071200c874dfd9a71501881ce31
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-03-23 14:42:26 +0000

    ROUTE: api updated with responses

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TicketController;
+use App\Http\Controllers\API\ResponseController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,11 +10,14 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
+
     Route::apiResource('tickets', TicketController::class)
-        ->where(['ticket' => '[0-9]+']);  
+        ->where(['ticket' => '[0-9]+']);
 
     Route::get('/tickets/available', [TicketController::class, 'availableTickets']);
-    Route::post('/tickets/{ticket}/claim', [TicketController::class, 'claim'])
-        ->where('ticket', '[0-9]+');
+    Route::post('/tickets/{ticket}/claim', [TicketController::class, 'claim']);
+    Route::patch('/tickets/{ticket}/resolve', [TicketController::class, 'resolve']);
+
+    Route::post('/tickets/{ticket}/responses', [ResponseController::class, 'store']);
+    Route::get('/tickets/{ticket}/responses', [ResponseController::class, 'index']);
 });
